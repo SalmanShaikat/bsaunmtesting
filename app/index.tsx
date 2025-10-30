3b7f5f88e35ecc5d368c0142293da4376b99d898
Salman Shaikat <95544564+SalmanShaikat@users.noreply.github.com>
2025-10-29 18:00:45 -0600

    Create index.tsx

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import HeroSection from './components/hero/HeroSection'
+
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      <HeroSection />
+      {/* Add more sections here later as chunks */}
+    </div>
+  )
+}
