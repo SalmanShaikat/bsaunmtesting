18196a3602acabfe62f51a1a3e695c7642b8c1c8
Salman Shaikat <95544564+SalmanShaikat@users.noreply.github.com>
2025-10-29 18:40:39 -0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './features/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'jungle-green': '#1a535c',
+        'seafoam-green': '#4ecdc4',
+        'unm-cherry': '#d90429',
+      },
+      fontFamily: {
+        sans: ['var(--font-inter)'],
+        serif: ['var(--font-georgia)'],
+      },
+    },
+  },
+  plugins: [],
+}
