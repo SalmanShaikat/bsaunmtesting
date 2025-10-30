75410afabaf05eb94b16bebd37e3f7e2205b892b
Salman Shaikat <95544564+SalmanShaikat@users.noreply.github.com>
2025-10-29 18:39:51 -0600

    Create constants.ts

---
+++ app/lib/constants.ts
@@ -0,0 +1,15 @@
+export const NAV_ITEMS = [
+  { name: 'BSA', href: '#home' },
+  { name: 'People', href: '#people' },
+  { name: 'Activity', href: '#activities' },
+  { name: 'Achievements', href: '#achievements' },
+  { name: 'Gallery', href: '#gallery' },
+  { name: 'Support Center', href: '#support' },
+] as const
+
+export const COLORS = {
+  jungleGreen: '#1a535c',
+  seafoamGreen: '#4ecdc4',
+  unmCherry: '#d90429',
+  lightBg: '#f7fff7',
+} as const
