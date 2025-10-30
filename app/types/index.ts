68e51f054f3412d83f74135f879437bef6321eff
Salman Shaikat <95544564+SalmanShaikat@users.noreply.github.com>
2025-10-29 18:40:14 -0600

    Create index.ts

---
+++ app/types/index.ts
@@ -0,0 +1,18 @@
+export interface Activity {
+  title: string
+  description: string
+  icon: string
+  color: string
+}
+
+export interface Achievement {
+  year: string
+  title: string
+  description: string
+  stats: string
+}
+
+export interface NavItem {
+  name: string
+  href: string
+}
